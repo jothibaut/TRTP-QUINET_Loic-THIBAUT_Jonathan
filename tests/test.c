8b296c1f75fd3ae715fcddad441964aa268a3397
loicq256 <loicq256@gmail.com>
2018-10-19 19:27:03 +0200

    avancer dans les tests

--- tests/test.c
+++ tests/test.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <zlib.h>
-#include "packet_interface.h"
+#include "../src/packet_interface.h"
 #include <string.h>
 #include <stdint.h>
 #include <arpa/inet.h>
@@ -17,31 +17,46 @@ int teardown(void) {
 	return 0;
 }
 
-void test_get_width(void){
-	struct fractal* test=new_fractal("name",800,800,0.8,-0.4);
-	CU_ASSERT_EQUAL(fractal_get_width(test),800);
-	CU_ASSERT_EQUAL(fractal_get_name(test),"name");
-	CU_ASSERT_EQUAL(fractal_get_height(test),800);
-	CU_ASSERT_EQUAL(fractal_get_a(test),0.8);
-	CU_ASSERT_EQUAL(fractal_get_b(test),-0.4);
+void test_get(void){
+	struct pkt* test=pkt_new();
+	b=0b01010100;
+	memcpy(test,&b,sizeof(uint8_t));
+	test->seqnum=0b01011011;
+	test->length=0b0000010110101101;
+	
+	CU_ASSERT_EQUAL(pkt_get_tr(test),0);
+	CU_ASSERT_EQUAL(pkt_get_type(test),01);
+	CU_ASSERT_EQUAL(pkt_get_window(test),10100);
+	CU_ASSERT_EQUAL(pkt_get_seqnum(test),0b01011011);
+	CU_ASSERT_EQUAL(pkt_get_length(test),0b0000010110101101);	
 }
 
 int main(){
 	CU_pSuite pSuite = NULL;
+	/* initialise le CPU du répertoire de test*/
 	if(CUE_SUCCESS!= CU_initialize_registry()){
 		return CU_get_error();
 	}
+	/* ajoute une suite au repertoire*/
 	pSuite = CU_add_suite("test",setup,teardown);
 	if(pSuite==NULL){
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
-	if( (NULL == CU_add_test(pSuite,"Test création fractale et retourne ces élements",test_get_width){
+	/* ajoute des teste à la suite*/
+	if( (NULL == CU_add_test(pSuite,"Test création fractale et retourne ces élements",test_get){
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
+	/*fait tourner les tests sur l'interface de base*/
+	CU_basic_set_mode(CU_BRM_VERBOSE); 
 	CU_basic_run_tests();
 	CU_basic_show_failures(CU_get_failure_list());
+	/* fait tourner les tests en utilisant l'interface automatisée*/
+ 	CU_automated_run_tests();
+ 	CU_list_tests_to_file();
+ 	/*fait tourner les tests en utilisant l'interface de controle*/
+	/*clean le répertoire et le retourne*/
 	CU_cleanup_registry();
 	return CU_get_error();
 }
