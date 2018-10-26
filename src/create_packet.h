376ed6db529a2bcb685a8ce8dfe194fc466c97c8
loicq256 <loicq256@gmail.com>
2018-10-26 17:34:32 +0200

    deconnexion implementé légère adaptation a faire

--- src/create_packet.h
+++ src/create_packet.h
@@ -3,6 +3,8 @@
 
 void create_packet_data(struct pkt *, char *, int, int);
 
+void create_packet_deco(struct pkt *thePkt, int seqnum);
+
 void create_packet_ack(struct pkt *, int);
 
 void create_packet_nack(struct pkt *, int);
